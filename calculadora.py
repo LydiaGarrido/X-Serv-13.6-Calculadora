27d19a2ab00c76b9d51f19e8b8336814939d8806
LydiaGarrido <l.garridomu@alumnos.urjc.es>
2018-02-02 13:41:30 +0100

    Corregidos errores pep8

--- calculadora.py
+++ calculadora.py
@@ -1,38 +1,40 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
 import sys
 
+
 def suma(operando1, operando2):
-	return operando1 + operando2
+    return operando1 + operando2
+
 
 def resta(operando1, operando2):
-	return operando1 - operando2
+    return operando1 - operando2
+
 
 def multiplicacion(operando1, operando2):
-	return operando1 * operando2
+    return operando1 * operando2
+
 
 def division(operando1, operando2):
-	return operando1 / operando2
+    return operando1 / operando2
 
 try:
-	operando1 = float(sys.argv[2])
-	operando2 = float(sys.argv[3])
+    operando1 = float(sys.argv[2])
+    operando2 = float(sys.argv[3])
 except ValueError:
-	sys.exit("No estan permitidos operandos que no sean float")
+    sys.exit("No estan permitidos operandos que no sean float")
 
 if sys.argv[1] == 'suma':
-	print(suma(operando1, operando2))
+    print(suma(operando1, operando2))
 
 elif sys.argv[1] == 'resta':
-	print(resta(operando1, operando2))
+    print(resta(operando1, operando2))
 
 elif sys.argv[1] == 'multiplicacion':
-	print(multiplicacion(operando1, operando2))
+    print(multiplicacion(operando1, operando2))
 
 elif sys.argv[1] == 'division':
-	try:
-		print(division(operando1, operando2))
-	except ZeroDivisionError:
-		sys.exit("No me dividas entre cero")
-
-
+    try:
+        print(division(operando1, operando2))
+    except ZeroDivisionError:
+        sys.exit("No me dividas entre cero")
